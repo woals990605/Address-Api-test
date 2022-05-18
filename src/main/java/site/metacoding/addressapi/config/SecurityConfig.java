2c2f8305709de6a654c480b365a77f3131e1383b
woals990605 <woals990605@naver.com>
2022-05-18 15:21:35 +0900

    시큐리티 인증 필터 설정

---
+++ src/main/java/site/metacoding/addressapi/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package site.metacoding.addressapi.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.authorizeRequests()
+                .antMatchers("/s/**").authenticated()
+                .anyRequest().permitAll()
+                .and()
+                .formLogin()
+                .loginPage("/loginForm")
+                .defaultSuccessUrl("/");
+    }
+}
