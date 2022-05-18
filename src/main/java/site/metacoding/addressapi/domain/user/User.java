294d64d8710870134fa4838a4636b5a87dcf7232
woals990605 <woals990605@naver.com>
2022-05-18 15:51:53 +0900

    모델링

---
+++ src/main/java/site/metacoding/addressapi/domain/user/User.java
@@ -0,0 +1,36 @@
+package site.metacoding.addressapi.domain.user;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@EntityListeners(AuditingEntityListener.class)
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 20, nullable = false, unique = true)
+    private String username;
+
+    @Column(length = 20, nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String address;
+
+}
