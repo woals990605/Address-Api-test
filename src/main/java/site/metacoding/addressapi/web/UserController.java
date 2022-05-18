63c8c1bf2a858eed731fc198ec02a4d6a3630f64
woals990605 <woals990605@naver.com>
2022-05-18 17:08:57 +0900

    css빼버림, 회원가입 완료

--- src/main/java/site/metacoding/addressapi/web/UserController.java
+++ src/main/java/site/metacoding/addressapi/web/UserController.java
@@ -1,13 +1,20 @@
 package site.metacoding.addressapi.web;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import lombok.RequiredArgsConstructor;
+import site.metacoding.addressapi.handler.ex.CustomException;
 import site.metacoding.addressapi.service.UserService;
+import site.metacoding.addressapi.web.dto.user.JoinReqDto;
 
 @RequiredArgsConstructor
 @Controller
@@ -26,7 +33,19 @@ public class UserController {
     }
 
     @PostMapping("/join")
-    public String join() {
+    public String join(@Valid JoinReqDto joinReqDto, BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            Map<String, String> errorMap = new HashMap<>();
+            for (FieldError fe : bindingResult.getFieldErrors()) {
+                errorMap.put(fe.getField(), fe.getDefaultMessage());
+            }
+
+            throw new CustomException(errorMap.toString());
+        }
+
+        userService.회원가입(joinReqDto.toEntity());
+
         return "redirect:/login-form";
     }
 }
